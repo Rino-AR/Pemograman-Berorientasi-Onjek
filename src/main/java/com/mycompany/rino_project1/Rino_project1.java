f89b0c4a31a1630e59207c9b7f57e2b861609652
Rino-AR <rinoahmadraga@gmail.com>
2025-08-26 17:00:41 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/rino_project1/Rino_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rino_project1;
+
+/**
+ *
+ * @author acer
+ */
+public class Rino_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Rino Ahmad Raga");
+        System.out.println("NPM : 2440506061");
+        System.out.println("Alamat : Magelang");
+        System.out.println("no_HP : 08595676812");
+    }
+}
